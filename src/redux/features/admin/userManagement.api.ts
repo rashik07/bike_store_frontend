2a437a035233e582f1642323b545d6ab984c654a
rashik07 <saif67090707@gmail.com>
2025-02-25 01:03:14 +0600

    update git error

--- src/redux/features/admin/userManagement.api.ts
+++ src/redux/features/admin/userManagement.api.ts
@@ -1,57 +1,10 @@
-import { TQueryParam, TResponseRedux, TStudent } from '../../../types';
+import { TQueryParam, TResponseRedux } from '../../../types';
 
 import { baseApi } from '../../api/baseApi';
 
 const userManagementApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    getAllStudents: builder.query({
-      query: (args) => {
-        console.log(args);
-        const params = new URLSearchParams();
-
-        if (args) {
-          args.forEach((item: TQueryParam) => {
-            params.append(item.name, item.value as string);
-          });
-        }
-
-        return {
-          url: '/students',
-          method: 'GET',
-          params: params,
-        };
-      },
-      transformResponse: (response: TResponseRedux<TStudent[]>) => {
-        return {
-          data: response.data,
-          meta: response.meta,
-        };
-      },
-    }),
-    getAllFaculties: builder.query({
-      query: (args) => {
-        console.log(args);
-        const params = new URLSearchParams();
-
-        if (args) {
-          args.forEach((item: TQueryParam) => {
-            params.append(item.name, item.value as string);
-          });
-        }
-
-        return {
-          url: '/faculties',
-          method: 'GET',
-          params: params,
-        };
-      },
-      transformResponse: (response: TResponseRedux<TStudent[]>) => {
-        return {
-          data: response.data,
-          meta: response.meta,
-        };
-      },
-    }),
+   
     signup: builder.mutation({
       query: (data) => ({
         url: '/users/create-customer',
@@ -71,7 +24,6 @@ const userManagementApi = baseApi.injectEndpoints({
 
 export const {
   useSignupMutation,
-  useGetAllStudentsQuery,
-  useGetAllFacultiesQuery,
+
   useChangePasswordMutation,
 } = userManagementApi;
